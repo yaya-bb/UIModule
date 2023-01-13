705339669ad1dfc50f070790f7824ba5ce198172
-yayabb <2286834433@qq.com>
2023-01-13 19:42:18 +0800

    feat:init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    globals: {
+      defineEmits: 'readonly',
+      defineProps: 'readonly'
+    },
+    extends: [
+      'plugin:vue/vue3-essential', // 这里需要注意 vue2 是`plugin:vue/essential` 
+      'standard',
+      'prettier',
+      'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      parser: '@typescript-eslint/parser',
+      sourceType: 'module'
+    },
+    plugins: ['vue', '@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  };
